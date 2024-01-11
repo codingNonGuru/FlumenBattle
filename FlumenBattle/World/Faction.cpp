1214af73ef9997ec3f19ded2cc5316e8c1245b28
codingNonGuru <andreialbu50@yahoo.com>
2024-01-11 23:00:40 +0200

    Add roll checks to Faction independence attempts

--- FlumenBattle/World/Faction.cpp
+++ FlumenBattle/World/Faction.cpp
@@ -1,5 +1,6 @@
 #include "Faction.h"
 #include "FlumenBattle/World/WorldScene.h"
+#include "FlumenBattle/Utility/Utility.h"
 
 using namespace world::polity;
 
@@ -20,8 +21,17 @@ FactionDecision Faction::Update()
 
     independenceDrive++;
 
-    if(independenceDrive < 100)
+    if(independenceDrive < 200)
+    {
         return {FactionDecisions::NONE};
+    }
+
+    auto revoltCheck = utility::RollD20Dice(15);
+    if(revoltCheck.IsAnyFailure() == true)
+    {
+        independenceDrive = 0;
+        return {FactionDecisions::NONE};
+    }
 
     return {FactionDecisions::DECLARE_INDEPENDENCE, this};
 }
