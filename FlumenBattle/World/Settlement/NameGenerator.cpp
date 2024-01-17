b67fd83957aad23b1f5498dc3d1bdf4af13836be
codingNonGuru <andreialbu50@yahoo.com>
2024-01-17 16:18:06 +0200

    Improve Settlement Name Generator

--- FlumenBattle/World/Settlement/NameGenerator.cpp
+++ FlumenBattle/World/Settlement/NameGenerator.cpp
@@ -5,8 +5,11 @@ using namespace world::settlement;
 static container::Array <Word> startParticles = 
 {
     "frost", "bill", "iron", "rock", "fire", "frame", "sour", "brim", "stone", "forge", "hack", "safe", "wood", "barren", "bare", "root",
-    "round", "even", "ever", "sweet", "bitter", "dour", "brisk", "well", "sun", "moon", "star", "sky", "flame", "wolf", "bear", "boar",
-    "groan", "grass", "old", "new", "near", "great", "bright", "dire", "wild", "fair"
+    "round", "even", "ever", "sweet", "bitter", "dour", "brisk", "well", "sun", "moon", "star", "sky", "flame", "wolf", "bear", "boar", "hare",
+    "groan", "grass", "old", "new", "near", "far", "great", "bright", "dire", "wild", "fair", "mill", "scrub", "flow", "silt", "kindle", "brave",
+    "dread", "hard", "tame", "dawn", "dusk", "mourn", "cheer", "bloom", "brine", "bold", "flood", "cliff", "home", "hair", "head", "gold",
+    "ferry", "merry", "sorrow", "song", "yarn", "chill", "shield", "yard", "breast", "fleck", "ice", "lone", "sage", "heart", "keen", "bale", "grim",
+    "shade", "wall", "blade", "spoke", "wise", "proud"
 };
 
 static container::Array <Word> endParticles = 
@@ -14,7 +17,9 @@ static container::Array <Word> endParticles =
     "ton", "town", "dike", "dyke", "ford", "land", "borrow", "borough", "kirk", "bury", "wick", "by", "bie", "mont", "berry", "ville", "court", "burn",
     "ground", "haven", "wood", "mark", "ham", "mane", "hall", "rock", "hill", "dam", "lane", "creek", "stone", "lake", "dale", "vale", "weald",
     "hurst", "meadow", "brook", "root", "spring", "plane", "bite", "shaft", "pit", "well", "field", "way", "road", "crack", "camp", "watch", "fort",
-    "bridge", "rim"
+    "bridge", "rim", "wind", "ridge", "pass", "rift", "pond", "lands", "water", "more", "cradle", "cliff", "head", "glade", "fen", "moor", "marsh",
+    "bog", "tip", "song", "yard", "path", "heart", "deep"
+    //"reef", "sea", "isle", "harbor", "dock", "tide", "flood", "coast", "beach", "wave"
 };
 
 Word NameGenerator::GenerateName()
